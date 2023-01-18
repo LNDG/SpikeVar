b1f3389efb6446950410c8eace138ec8f7d963f5
kamp <fbn.kamp@gmail.com>
2023-01-18 09:57:12 +0100

    debugging

--- stimuli/get_stim_learn_idx.m
+++ stimuli/get_stim_learn_idx.m
@@ -2,6 +2,6 @@ function stim_learn_idx = get_stim_learn_idx(all_HMAX_info, fulltable)
     %% get indices of learned stimuli
     %% ordered as they were presented to subjects
     for il = 1:size(fulltable,1)
-        stim_learn_idx(il) = find(all_HMAX_info.StimulusCode==fulltable.stimuliLearn(il));
+        stim_learn_idx(il) = find(all_HMAX_info.stimulus_code==fulltable.stimuliLearn(il));
     end
 end
